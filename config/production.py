96b359a8faefecbe2265db44ba89b7ba6f07276c
e-plate <lg24112077@gmail.com>
2021-08-13 13:49:00 +0900

    config 변경

--- config/production.py
+++
@@ -1,5 +0,0 @@
-from config.default import *
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'Q=\xd7\x8e\xf9\xa5n\xd2C\x02\x12]-x T'
--- pybo/__init__.py
+++ pybo/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flaskext.markdown import Markdown
-
 from sqlalchemy import MetaData
 
 import config
@@ -17,31 +16,21 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
-# --------------------------------------------------------------------------- #
+
 def create_app():
     app = Flask(__name__)
-    app.config.from_envvar('APP_CONFIG_FILE')
+    app.config.from_object(config)
 
     # ORM
     db.init_app(app)
-
     if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
         migrate.init_app(app, db, render_as_batch=True)
     else:
         migrate.init_app(app, db)
-
-
-# --------------------------------- [edit] ---------------------------------- #
     from . import models
-# --------------------------------------------------------------------------- #
-
-
 
     # 블루프린트
-
-# --------------------------------- [edit] ---------------------------------- #
     from .views import main_views, question_views, answer_views, auth_views, comment_views, vote_views
-# --------------------------------------------------------------------------- #
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
@@ -49,17 +38,11 @@ def create_app():
     app.register_blueprint(comment_views.bp)
     app.register_blueprint(vote_views.bp)
 
-# --------------------------------- [edit] ---------------------------------- #
     # 필터
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
-    # --------------------------------------------------------------------------- #
-    # --------------------------------- [edit] ---------------------------------- #
+
     # markdown
     Markdown(app, extensions=['nl2br', 'fenced_code'])
-    # --------------------------------------------------------------------------- #
-
-
-
 
     return app

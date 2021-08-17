562cab615fbf3247a71c6d5ac37507768b1491de
e-plate <lg24112077@gmail.com>
2021-08-17 13:57:18 +0900

    config 변경

--- config/production.py
+++ config/production.py
@@ -2,5 +2,5 @@ from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b"\xd7uJ\r>%lf\x9e\x93\xf4^'\xee\xc4#"
+SECRET_KEY = b'\x19h\x8eR\x84f\xd1\xbc\x0f\x11vpH\xd3N\x04'
 
